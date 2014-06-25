05d8bf7a5c8b5ed8fd2ad0b1d7ae4e9c4d78a858
csernaque <carlossernaqch@hotmail.com>
2014-06-24 22:39:09 -0500

    mostrando el index.phtml

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -1,53 +1,16 @@
 <?php
-namespace Album;
+namespace Album\Model;
 
- use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
- use Zend\ModuleManager\Feature\ConfigProviderInterface;
- // Add these import statements:
- use Album\Model\Album;
- use Album\Model\AlbumTable;
- use Zend\Db\ResultSet\ResultSet;
- use Zend\Db\TableGateway\TableGateway;
+class Album
+{
+    public $id;
+    public $artist;
+    public $title;
 
-
- class Module implements AutoloaderProviderInterface, ConfigProviderInterface
- {
-     public function getAutoloaderConfig()
-     {
-         return array(
-             'Zend\Loader\ClassMapAutoloader' => array(
-                 __DIR__ . '/autoload_classmap.php',
-             ),
-             'Zend\Loader\StandardAutoloader' => array(
-                 'namespaces' => array(
-                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                 ),
-             ),
-         );
-     }
-
-     public function getConfig()
-     {
-         return include __DIR__ . '/config/module.config.php';
-     }
-// Add this method:     
-     public function getServiceConfig()
-     {
-         return array(
-             'factories' => array(
-                 'Album\Model\AlbumTable' =>  function($sm) {
-                     $tableGateway = $sm->get('AlbumTableGateway');
-                     $table = new AlbumTable($tableGateway);
-                     return $table;
-                 },
-                 'AlbumTableGateway' => function ($sm) {
-                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
-                     $resultSetPrototype = new ResultSet();
-                     $resultSetPrototype->setArrayObjectPrototype(new Album());
-                     return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
-                 },
-             ),
-         );
-     }
-
- }
+    public function exchangeArray($data)
+    {
+        $this->id     = (isset($data['id'])) ? $data['id'] : null;
+        $this->artist = (isset($data['artist'])) ? $data['artist'] : null;
+        $this->title  = (isset($data['title'])) ? $data['title'] : null;
+    }
+}
