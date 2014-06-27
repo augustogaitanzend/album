51fbca3ebf3d3bedeef90c739458d7b2722d35ff
csernaque <carlossernaqch@hotmail.com>
2014-06-27 03:06:41 -0500

    formulario edit

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -19,11 +19,11 @@ class Album implements InputFilterAwareInterface
         $this->artist = (isset($data['artist'])) ? $data['artist'] : null;
         $this->title  = (isset($data['title']))  ? $data['title']  : null;
     }
+    
     public function getArrayCopy()
     {
         return get_object_vars($this);
     }
-
     // Add content to this method:
     public function setInputFilter(InputFilterInterface $inputFilter)
     {
@@ -86,9 +86,5 @@ class Album implements InputFilterAwareInterface
         }
 
         return $this->inputFilter;
-    }
-    public function getArrayCopy()
-    {
-        return get_object_vars($this);
     }
 }
